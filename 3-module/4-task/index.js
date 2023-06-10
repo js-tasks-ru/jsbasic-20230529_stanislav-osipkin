3200781af885cbefe41ed724b18ccb13986eb811
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-10 22:04:31 +0500

    Module 3 task 5 is completed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  return users.filter(user => user.age <= age)
+  .map((user) => `${user.name} ${user.balance}`)
+  .forEach((user, index) => index < (users.length - 1) ? user + '\n' : user);
+
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  arr = str.split(' ').filter(item => isFinite(item));
+
+  result['min'] = Math.min(...arr);
+  result['max'] = Math.max(...arr);
+
+  return result;
+
 }
+
