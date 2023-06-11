1e74f7f4aeb5d78333253ff5c805444031cddd9e
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-11 21:14:08 +0500

    Module 3 task 4 for checking

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,6 +1,6 @@
 function showSalary(users, age) {
   return users.filter(user => user.age <= age)
-  .map((user) => `${user.name} ${user.balance}`)
-  .forEach((user, index) => index < (users.length - 1) ? user + '\n' : user);
-
+  .map((user) => `${user.name}, ${user.balance}`)
+  .reduce((sum, user, index) => sum + ((index < users.length - 1)
+    ? `${user}\n` : `${user}`), '');
 }
