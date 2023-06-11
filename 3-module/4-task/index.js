6c80ea5268f65719624f8326370cd3152d61fa49
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-11 22:30:33 +0500

    Module 3 task 5 - mistake is fixed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,6 +1,5 @@
 function showSalary(users, age) {
   return users.filter(user => user.age <= age)
   .map((user) => `${user.name}, ${user.balance}`)
-  .reduce((sum, user, index) => sum + ((index < users.length - 1)
-    ? `${user}\n` : `${user}`), '');
+  .join('\n');
 }
