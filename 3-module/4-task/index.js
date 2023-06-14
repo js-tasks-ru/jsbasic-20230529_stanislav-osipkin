9b5ed2bea8111a67424f5bd61c3f23dbd8974cd6
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-14 22:09:06 +0500

    Module 4 tasks 1,2 are completed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -3,3 +3,11 @@ function showSalary(users, age) {
   .map((user) => `${user.name}, ${user.balance}`)
   .join('\n');
 }
+
+// function showSalary(users, age) {
+//   let arr = users.filter(user => user.age <= age)
+//   .map(user => `${user.name}, ${user.balance}`);
+//   return arr.reduce((sum, item, index) => sum + ((index < arr.length - 1)
+//     ? `${item}\n` : `${item}`), '');
+
+// }
