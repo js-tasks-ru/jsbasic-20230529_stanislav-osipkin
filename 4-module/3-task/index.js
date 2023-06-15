d458cc8dc048b2bb5f8e9469483cb7c966736809
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-15 17:03:48 +0500

    Module 4 task 3 for checking

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,41 @@
 function highlight() {
-  // ваш код...
+  let trList = table.children[1].rows;
+
+  for (let tr of trList) {
+    // data-available
+    if (tr.cells[3].dataset.available === 'true') {
+      tr.classList.add('available');
+    }
+    else if (tr.cells[3].dataset.available === 'false') {
+      tr.classList.add('unavailable');
+    }
+    else if (!tr.cells[3].dataset.available) { tr.hidden = true; }
+
+    // gender
+    if (tr.cells[2].textContent === 'm') {
+      tr.classList.add('male');
+    }
+
+    else if (tr.cells[2].textContent === 'f') {
+      tr.classList.add('female');
+    }
+
+    // inline-стиль
+    if (tr.cells[1].textContent < 18) {
+      tr.style.textDecoration = 'line-through';
+    }
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
