629f7e963c1548cbee713722ad59a6518a8e3df7
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-22 20:07:24 +0500

    Module 5 task 3 for checking

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+
+  let container = document.querySelector('.container');
+  let slide = document.querySelector('.carousel__inner');
+  let carouselArr = Array.from(document.querySelectorAll('.carousel__slide'));
+  let offset = 0;
+
+  container.addEventListener('click', function (event) {
+    let nextArrow = event.target.closest('.carousel__arrow_right');
+    let prevArrow = event.target.closest('.carousel__arrow_left');
+
+
+    if (nextArrow) {
+      offset -= slide.offsetWidth;
+      if (offset <= -((carouselArr.length - 1) * slide.offsetWidth)) {
+        nextArrow.style.display = 'none';
+      }
+      else { nextArrow.style.display = ''; }
+      console.log(offset);
+
+      slide.style.transform = `translateX(${offset}px)`;
+    }
+
+    if (prevArrow) {
+      offset += slide.offsetWidth;
+      if (offset >= 0) {
+        prevArrow.style.display = 'none';
+      }
+      console.log(offset);
+
+      slide.style.transform = `translateX(${offset}px)`;
+    }
+  });
 }
+
