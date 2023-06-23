8c8c95a4c16f58267a0c91e4474c1b0d0b2e2324
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-23 12:25:13 +0500

    Module 5 task 5 is completed

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -3,30 +3,37 @@ function initCarousel() {
   let container = document.querySelector('.container');
   let slide = document.querySelector('.carousel__inner');
   let carouselArr = Array.from(document.querySelectorAll('.carousel__slide'));
+  let slideCount = 0;
   let offset = 0;
+  let nextArrow = document.querySelector('.carousel__arrow_right');
+  let prevArrow = document.querySelector('.carousel__arrow_left');
+  document.querySelector('.carousel__arrow_left').style.display = 'none';
 
   container.addEventListener('click', function (event) {
-    let nextArrow = event.target.closest('.carousel__arrow_right');
-    let prevArrow = event.target.closest('.carousel__arrow_left');
+    let next = event.target.closest('.carousel__arrow_right');
+    let prev = event.target.closest('.carousel__arrow_left');
 
 
-    if (nextArrow) {
+    if (next) {
+      slideCount++;
+      prevArrow.style.display = '';
       offset -= slide.offsetWidth;
-      if (offset <= -((carouselArr.length - 1) * slide.offsetWidth)) {
+
+      if (slideCount === (carouselArr.length - 1)) {
         nextArrow.style.display = 'none';
       }
-      else { nextArrow.style.display = ''; }
-      console.log(offset);
 
       slide.style.transform = `translateX(${offset}px)`;
     }
 
-    if (prevArrow) {
+    if (prev) {
+      slideCount--;
+      nextArrow.style.display = '';
       offset += slide.offsetWidth;
-      if (offset >= 0) {
+
+      if (slideCount === 0) {
         prevArrow.style.display = 'none';
       }
-      console.log(offset);
 
       slide.style.transform = `translateX(${offset}px)`;
     }
