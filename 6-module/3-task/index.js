d4fa5ce5f05cff1d814ab688e032b40150f37b1e
Stanislav Osipkin <stanislav.osipkin@gmail.com>
2023-06-26 22:32:50 +0500

    Module 6 task 3 corrected Onclick and slideToggle

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -4,8 +4,7 @@ export default class Carousel {
   constructor(slides) {
     this.slides = slides;
     this.render();
-    this.customEvent();
-    // this.slideToggle();
+    this.slideToggle();
   }
 
   render() {
@@ -21,15 +20,16 @@ export default class Carousel {
     </div>
     `);
     this.elem.querySelector('.carousel__inner').insertAdjacentHTML('beforeend',
-      this.slides.map(({image, price, name}) =>
-        `<div class="carousel__slide" data-id="penang-shrimp">
-            <img src="../../assets/images/carousel/${image}" class="carousel__img" alt="slide">
-            <div class="carousel__caption">
+      this.slides.map(({id, image, price, name}) =>
+        `<div class="carousel__slide" data-id="${id}">
+          <img src="../../assets/images/carousel/${image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
             <span class="carousel__price">€${price.toFixed(2)}</span>
             <div class="carousel__title">${name}</div>
             <button type="button" class="carousel__button">
               <img src="/assets/images/icons/plus-icon.svg" alt="icon">
             </button>
+          </div>
          </div>
         `).join('')
     );
@@ -41,61 +41,53 @@ export default class Carousel {
 
   Onclick = () => {
     let myEvent = new CustomEvent('product-add', {
-      detail: this.slides,
+      detail: this.elem.querySelector('.carousel__slide').dataset.id,
       bubbles: true,
     });
 
     this.elem.dispatchEvent(myEvent);
   }
 
-  customEvent() {
-    this.elem.addEventListener('product-add', event => {
-      console.log(event.detail);
 
+  slideToggle() {
+    this.container = document.querySelector('.container');
+    this.slide = this.elem.querySelector('.carousel__inner');
+    this.carouselArr = Array.from(this.elem.querySelectorAll('.carousel__slide'));
+    this.slideCount = 0;
+    this.offset = 0;
+    this.nextArrow = this.elem.querySelector('.carousel__arrow_right');
+    this.prevArrow = this.elem.querySelector('.carousel__arrow_left');
+    this.prevArrow.style.display = 'none';
 
+    this.container.addEventListener('click', function (event) {
+      this.next = event.target.closest('.carousel__arrow_right');
+      this.prev = event.target.closest('.carousel__arrow_left');
+
+      if (this.next) {
+        this.slideCount++;
+        this.prevArrow.style.display = '';
+        this.offset -= this.slide.offsetWidth;
+
+        if (this.slideCount === (this.carouselArr.length - 1)) {
+          this.nextArrow.style.display = 'none';
+        }
+
+        this.slide.style.transform = `translateX(${offset}px)`;
+      }
+
+      if (this.prev) {
+        this.slideCount--;
+        this.nextArrow.style.display = '';
+        this.offset += this.slide.offsetWidth;
+
+        if (this.slideCount === 0) {
+          this.prevArrow.style.display = 'none';
+        }
+
+        this.slide.style.transform = `translateX(${offset}px)`;
+      }
     });
   }
-
-  // slideToggle() {
-
-  //   this.carousel = this.elem.closest('.carousel');
-  //   this.slide = this.carousel.querySelector('.carousel__inner');
-  //   this.carouselArr = Array.from(this.carousel.querySelectorAll('.carousel__slide'));
-  //   this.slideCount = 0;
-  //   this.offset = 0;
-  //   this.nextArrow = this.carousel.querySelector('.carousel__arrow_right');
-  //   this.prevArrow = this.carousel.querySelector('.carousel__arrow_left').style.display = 'none';
-  //   console.log(this.slide.offsetWidth);
-
-  //   this.carousel.addEventListener('click', function (event) {
-  //     this.next = event.target.closest('.carousel__arrow_right');
-  //     this.prev = event.target.closest('.carousel__arrow_left');
-
-  //     if (this.next) {
-  //       this.slideCount++;
-  //       this.prevArrow.style.display = '';
-  //       this.offset -= this.slide.offsetWidth;
-
-  //       if (this.slideCount === (this.carouselArr.length - 1)) {
-  //         this.nextArrow.style.display = 'none';
-  //       }
-
-  //       this.slide.style.transform = `translateX(${offset}px)`;
-  //     }
-
-  //     if (this.prev) {
-  //       this.slideCount--;
-  //       this.nextArrow.style.display = '';
-  //       this.offset += this.slide.offsetWidth;
-
-  //       if (this.slideCount === 0) {
-  //         this.prevArrow.style.display = 'none';
-  //       }
-
-  //       this.slide.style.transform = `translateX(${offset}px)`;
-  //     }
-  //   });
-  // }
 
 
 }
